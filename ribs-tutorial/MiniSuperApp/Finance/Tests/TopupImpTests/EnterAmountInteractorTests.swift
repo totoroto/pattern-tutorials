e454cf80dbb04817ee6844152657158c5d26da69
totoroto <lsb0496@naver.com>
2024-03-27 08:42:45 +0900

    EnterAmountInteractor의 activate, didTapTopup에 대한 테스트 코드 작성

--- ribs-tutorial/MiniSuperApp/Finance/Tests/TopupImpTests/EnterAmountInteractorTests.swift
+++ ribs-tutorial/MiniSuperApp/Finance/Tests/TopupImpTests/EnterAmountInteractorTests.swift
@@ -7,6 +7,8 @@
 
 @testable import TopupImp
 import XCTest
+import FinanceEntity
+import FinanceRepositoryTestSupport
 
 final class EnterAmountInteractorTests: XCTestCase {
 
@@ -14,7 +16,9 @@ final class EnterAmountInteractorTests: XCTestCase {
     private var presenter: EnterAmountPresentableMock!
     private var dependency: EnterAmountDependencyMock!
     private var listener: EnterAmountListenerMock!
-
+    private var repository: SuperPayRepositoryMock {
+        dependency.superPayRepository as! SuperPayRepositoryMock
+    }
     // TODO: declare other objects and mocks you need as private vars
 
     override func setUp() {
@@ -33,9 +37,42 @@ final class EnterAmountInteractorTests: XCTestCase {
 
     func testActivate() {
         // given
+        let paymentMethod = PaymentMethod(id: "id0",
+                                          name: "name0",
+                                          digits: "0000",
+                                          color: "#FFFFFF",
+                                          isPrimary: false)
+        dependency.selectedPaymentMethodSubject.send(paymentMethod)
         
         // when
+        sut.activate()
         
         // then
+        XCTAssertEqual(presenter.updateSelectedPaymentMethodCallCount, 1)
+        XCTAssertEqual(presenter.updateSelectedPaymentMethodViewModel?.name, "name0 0000")
+        XCTAssertNotNil(presenter.updateSelectedPaymentMethodViewModel?.image)
+    }
+    
+    func testTopupWithValidAmount() {
+        // given
+        let paymentMethod = PaymentMethod(id: "id0",
+                                          name: "name0",
+                                          digits: "0000",
+                                          color: "#FFFFFF",
+                                          isPrimary: false)
+        dependency.selectedPaymentMethodSubject.send(paymentMethod)
+        
+        // when
+        sut.didTapTopup(with: 1_000_000)
+        
+        // then
+        _ = XCTWaiter.wait(for: [expectation(description: "Wait 0.1s")], timeout: 0.1)
+        
+        XCTAssertEqual(presenter.startLoadingCallCount, 1)
+        XCTAssertEqual(presenter.stopLoadingCallCount, 1)
+        XCTAssertEqual(repository.topupCallCount, 1)
+        XCTAssertEqual(repository.topupPaymentID, "id0")
+        XCTAssertEqual(repository.topupAmount, 1_000_000)
+        XCTAssertEqual(listener.enterAmountDidFinishTopupCallCount, 1)
     }
 }
